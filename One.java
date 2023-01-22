26f04f60a6e4f510997a310fffe1bd2f0dd78ab5
SUKAD3V <sukadevmurmu2020@gmail.com>
2023-01-22 08:40:44 -0500

    first commit

---
+++ One.java
@@ -0,0 +1,12 @@
+public class One{
+  public static void main(String[] args){
+    boolean isDamaged = false;
+    String dbName = "sukadev";
+    String dbPass = "1234";
+    if( dbName.equals("sukadev") && dbPass.equals("1234")){
+      System.out.println("your loged in");
+    }else{
+      System.out.println("Please Provide correct login Details");
+    }     
+  }
+  }
